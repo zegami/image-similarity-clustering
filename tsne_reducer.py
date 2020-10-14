87357e38b749168c806ed57614e08ad9771b191c
Doug <douglawrence0010@gmail.com>
2020-10-14 15:12:28 +0100

    Upgraded API for tsne and umap functions, now consistent and support all respective arguments with kwargs

--- tsne_reducer.py
+++ tsne_reducer.py
@@ -7,23 +7,48 @@ Created on Wed Mar 18 22:55:14 2020
 
 from sklearn.manifold import TSNE
 import pandas as pd
+
+
+# There are many default parameters, but the ones exposed here should be
+# tailored to the project specifically. These can be entered in the umap
+# function below as keyword arguments. Query TSNE docstring for more options
+DEFAULT_TSNE_KWARGS = {
+    
+    # The number of dimensions to reduce into. Typically 2, to create
+    # coordinates appropriate for an X/Y graph, but can reasonably be anything
+    # from 2 -> 100. For 3D plottable coordinates, use 3.
+    'n_components' : 2,
+    
+    
+    # Having a learning rate too high will cause the results to form a 'ball',
+    # too low causes compressed dense clouds with few outliers.
+    'learning_rate' : 200.0 # Typically 10.0 -> 1000.0
+}
         
 
-def tsne(features, dims=2, write_to=None, tsne_kwargs={}):
-    ''' Reduces the features in the parsed pd.DataFrame 'features' into 'dims'
+def tsne(features, write_to=None, **tsne_kwargs):
+    ''' Reduces the features in the parsed pd.DataFrame 'features' into less
     dimensions (default 2). Writes the output to 'write_to' if provided, in
     .csv format. Returns the feature DataFrame.
+    
+    Provide any extra tsne keyword arguments as needed (query TSNE docstring
+    to find these). These will override DEFAULT_TSNE_KWARGS.
     '''
     
+    # Use the provided tsne arguments on top of the defaults above
+    kwargs = DEFAULT_TSNE_KWARGS.copy()
+    for key, val in tsne_kwargs.items():
+        kwargs[key] = val
+        print('[Custom TSNE argument - {}: {}]'.format(key, val))
+    
     id_col_name = features.columns[0]
-    tsne_kwargs['n_components'] = dims
         
-    print('t-SNE: Reducing features to {} dimensions'.format(dims))
+    print(f't-SNE: Reducing features to {kwargs["n_components"]} dimensions')
     
     # Don't consider the first unique ID column
     features_salient = features.copy().drop(columns=[id_col_name], axis=1)
     
-    reduced = pd.DataFrame(TSNE(**tsne_kwargs).fit_transform(features_salient), dtype=object)
+    reduced = pd.DataFrame(TSNE(**kwargs).fit_transform(features_salient), dtype=object)
     reduced.insert(0, id_col_name, features[[id_col_name]])
     
     print('Success')
--- umap_reducer.py
+++ umap_reducer.py
@@ -11,8 +11,14 @@ import pandas as pd
 
 # There are many default parameters, but the ones exposed here should be
 # tailored to the project specifically. These can be entered in the umap
-# function below as keyword arguments
+# function below as keyword arguments. Query UMAP docstring for more options
 DEFAULT_UMAP_KWARGS = {
+    
+    # The number of dimensions to reduce into. Typically 2, to create
+    # coordinates appropriate for an X/Y graph, but can reasonably be anything
+    # from 2 -> 100. For 3D plottable coordinates, use 3.
+    'n_components' : 2,
+    
     
     # Use 'categorical' for categorised data not on a continuum, like types
     # of plants, etc. 'l1' or 'l2' are more appropriate for continuous values,
@@ -21,10 +27,13 @@ DEFAULT_UMAP_KWARGS = {
 }
 
 
-def umap(features, dims=2, write_to=None, **umap_kwargs):
-    ''' Reduces the features in the parsed pd.DataFrame 'features' into 'dims'
+def umap(features, write_to=None, **umap_kwargs):
+    ''' Reduces the features in the parsed pd.DataFrame 'features' into less
     dimensions (default 2). Writes the output to 'write_to' if provided, in
-    .csv format. Returns the feature DataFrame.
+    .csv format. Returns the reduced feature DataFrame.
+    
+    Provide any extra umap keyword arguments as needed (query UMAP docstring
+    to find these). These will override DEFAULT_UMAP_KWARGS
     '''
     
     # Use the provided umap arguments on top of the defaults above
@@ -33,12 +42,9 @@ def umap(features, dims=2, write_to=None, **umap_kwargs):
         kwargs[key] = val
         print('[Custom UMAP argument - {}: {}]'.format(key, val))
     
-    if dims != 2:
-        print('UMAP: Not currently supporting anything but 2-dim reduction')
-    
     id_col_name = features.columns[0]
         
-    print('UMAP: Reducing features to {} dimensions'.format(dims))
+    print(f'UMAP: Reducing features to {kwargs["n_components"]} dimensions')
     
     # Don't consider the first unique ID column
     features_salient = features.copy().drop(columns=[id_col_name], axis=1)
